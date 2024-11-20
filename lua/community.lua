34cb4a4299cfa1c44a2db3779b940979545cdb07
Bradley Newton <bnewton@vcinity.io>
2024-11-20 14:45:47 -0800

    adds bash lsp functionality

--- lua/community.lua
+++ lua/community.lua
@@ -6,6 +6,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   -- import/override with your plugins folder
