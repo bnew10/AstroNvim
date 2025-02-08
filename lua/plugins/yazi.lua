39f298126763d7b49edcdac89c82b82d3e622326
Bradley Newton <bnewton@vcinity.io>
2025-02-07 18:16:01 -0800

    adds yazi.nvim plugin

---
+++ lua/plugins/yazi.lua
@@ -0,0 +1,28 @@
+---@type LazySpec
+return {
+  "mikavilpas/yazi.nvim",
+  dependencies = {
+    {
+      "AstroNvim/astrocore",
+      opts = function(_, opts)
+        local maps = assert(opts.mappings)
+        maps.n["<Leader>y"] = { "<cmd>Yazi<cr>", desc = "Yazi" }
+      end,
+    },
+  },
+  cmd = "Yazi",
+  ---@type YaziConfig
+  opts = {
+    open_for_directories = false,
+    keymaps = {
+      show_help = "<f1>",
+      cycle_open_buffers = "<a-tab>",
+      grep_in_directory = false,
+    },
+    -- yazi_floating_window_border = "none",
+    future_features = {
+      ya_emit_reveal = true,
+      ya_emit_open = true,
+    },
+  },
+}
