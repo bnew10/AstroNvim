b8eb5e7825443a4b8d3e51080c377b74dd1e1425
Bradley Newton <bradnew10@gmail.com>
2025-04-07 03:57:01 -0700

    adds telescope to astrocommunity file

--- lua/community.lua
+++ lua/community.lua
@@ -18,6 +18,7 @@ return {
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.completion.avante-nvim" },
+  { import = "astrocommunity.fuzzy-finder.telescope-nvim" },
   -- { import = "astrocommunity.debugging.nvim-dap-virtual-text" },
   { import = "astrocommunity.recipes.astrolsp-no-insert-inlay-hints" },
   { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
